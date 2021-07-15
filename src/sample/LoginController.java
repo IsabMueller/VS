ecd5cc203b146cc596bbb710ea58e4cbb2aeef2d
mharasch <mharasch>
2021-07-15 18:54:00 +0200

    ID's für Reg hinzugefügt

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -58,4 +58,5 @@ public class LoginController implements Initializable {
     public void validateLogin(){
 
     }
+
 }
--- src/sample/RegisterController.java
+++ src/sample/RegisterController.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+import java.io.File;
+import java.net.URL;
+
 public class RegisterController {
+
 }
