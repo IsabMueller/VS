d144a7dbcda4d585e1e457e0a7f5ddcc951a7720
Haidar <haidarisso98@gmail.com>
2021-08-19 05:21:11 +0200

    Email Passwort Controller

--- src/sample/EinstellungenAendernPasswortController.java
+++ src/sample/EinstellungenAendernPasswortController.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
+
 public class EinstellungenAendernPasswortController {
+
+    @FXML
+    private AnchorPane PasswortChangePane;
+    @FXML
+    private Button btnBackSettings;
+    @FXML
+    private Button btnChangedPasswort;
+
+    public void handleBtnBackSettings(ActionEvent event) throws IOException {
+        AnchorPane newPane = FXMLLoader.load(getClass().getResource("SicherheitsEinstellungen.fxml"));
+        PasswortChangePane.getChildren().setAll(newPane);
+    }
 }
