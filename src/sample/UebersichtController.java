7572039423e4a87b278a40352aa2c1ca21810661
Haidar <haidarisso98@gmail.com>
2021-08-19 06:58:22 +0200

    TerminBearbeiten Controller

--- src/sample/UebersichtController.java
+++ src/sample/UebersichtController.java
@@ -10,12 +10,40 @@ import java.io.IOException;
 
 public class UebersichtController {
     @FXML
+    private Button btnTagesübersicht;
+    @FXML
+    private Button btnMonatsübersicht;
+    @FXML
+    private Button btnWochenübersicht;
+    @FXML
+    private Button btnJahresübersicht;
+    @FXML
     private AnchorPane uebersichtPane;
     @FXML
     private Button btnBackUebersicht;
 
     public void handleBtnBackUebersicht(ActionEvent event) throws IOException {
         AnchorPane newPane = FXMLLoader.load(getClass().getResource("Start.fxml"));
+        uebersichtPane.getChildren().setAll(newPane);
+    }
+
+    public void handleBtnTagesübersicht(ActionEvent event) throws IOException {
+        AnchorPane newPane = FXMLLoader.load(getClass().getResource("TagesÜbersicht.fxml"));
+        uebersichtPane.getChildren().setAll(newPane);
+    }
+
+    public void handleBtnWochenübersicht(ActionEvent event) throws IOException {
+        AnchorPane newPane = FXMLLoader.load(getClass().getResource("WochenÜbersicht.fxml"));
+        uebersichtPane.getChildren().setAll(newPane);
+    }
+
+    public void handleBtnMonatsübersicht(ActionEvent event) throws IOException {
+        AnchorPane newPane = FXMLLoader.load(getClass().getResource("MonatsÜbersicht.fxml"));
+        uebersichtPane.getChildren().setAll(newPane);
+    }
+
+    public void handleBtnJahresübersicht(ActionEvent event) throws IOException {
+        AnchorPane newPane = FXMLLoader.load(getClass().getResource("JahresÜbersicht.fxml"));
         uebersichtPane.getChildren().setAll(newPane);
     }
 }
