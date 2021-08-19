1a80ba3ead77796279d5443b7932ff2b075477df
Haidar <haidarisso98@gmail.com>
2021-08-19 04:50:56 +0200

    Email Passwort Controller

--- src/sample/EinstellungenAendernEmailController.java
+++ src/sample/EinstellungenAendernEmailController.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class EinstellungenAendernEmailController {
+    @FXML
+    private Button btnBackSettings;
+    @FXML
+    private Button btnChangedEmail;
 }
