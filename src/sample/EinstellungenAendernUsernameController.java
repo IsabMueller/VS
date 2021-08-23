59798459e0eef27b7c9f9ad37c450eff51b54440
Haidar <haidarisso98@gmail.com>
2021-08-24 00:57:03 +0200

    Textfeld verknüfung Userneáme ändern

--- src/sample/EinstellungenAendernUsernameController.java
+++ src/sample/EinstellungenAendernUsernameController.java
@@ -4,12 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
 public class EinstellungenAendernUsernameController {
+    @FXML
+    private TextField tfChangedUsername;
     @FXML
     private Button btnBackSettings;
     @FXML
@@ -23,5 +26,16 @@ public class EinstellungenAendernUsernameController {
     public void handleBtnBackSettings(ActionEvent event) throws IOException {
         AnchorPane newPane = FXMLLoader.load(getClass().getResource("ProfilEinstellungen.fxml"));
         usernameChangePane.getChildren().setAll(newPane);
+    }
+     //Textfeld Eingabe Auswerten
+    String UsernameEingabe;
+
+    public TextField getTfChangedUsername() {
+        return tfChangedUsername;
+    }
+
+    public void submit(ActionEvent event) {
+        UsernameEingabe = tfChangedUsername.getText();
+        System.out.println(UsernameEingabe);
     }
 }
