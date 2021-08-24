018234be03f28476df4633637df04ad47ee776fa
IsabMueller <ki.asja.mueller@htwsaar.de>
2021-08-24 04:34:09 +0200

    temp ft speichert in liste

--- src/sample/controller/KategorienVerwaltenController.java
+++ src/sample/controller/KategorienVerwaltenController.java
@@ -1,18 +1,34 @@
 package sample.controller;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
+
+
 public class KategorienVerwaltenController {
+    @FXML
+    private Button btnKategorienAdd;
+    @FXML
+    private ColorPicker colorpickerKategorien;
+    @FXML
+    private ListView listKategorie;
+    @FXML
+    private TextField tfKategorieName;
     @FXML
     private Button btnBackKategorienVerwalten;
     @FXML
     private AnchorPane KategorienVerwaltenPane;
+    private String https;
 
     /**
      *
@@ -23,4 +39,22 @@ public class KategorienVerwaltenController {
         AnchorPane newPane = FXMLLoader.load(getClass().getResource("../view/Einstellungen.fxml"));
         KategorienVerwaltenPane.getChildren().setAll(newPane);
     }
+
+    String kategorie;
+
+    public TextField getTfKategorieName(){
+        return tfKategorieName;
+    }
+
+    public void submitKat(ActionEvent event) {
+        kategorie = tfKategorieName.getText();
+        System.out.println(kategorie);
+
+        listKategorie.getItems().addAll(kategorie);
+
+
+
+    }
+
+
 }
